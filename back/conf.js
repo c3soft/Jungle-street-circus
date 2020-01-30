ad14f370f993d0a807e54ba2ceb37681152caaa8
Pascal <fedodev55@gmail.com>
2020-01-30 09:59:09 +0100

    delete conf back

--- back/conf.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Rootroot@123',
-  database: 'opus'
-});
-module.exports = connection;
